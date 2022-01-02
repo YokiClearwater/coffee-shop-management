c7be8897cc375768eec27ec1e3c9047b2bb55004
tinker <chandara12122001@gmail.com>
2022-01-02 14:10:17 +0700

    a

--- ForUserLogIn.cpp
+++ ForUserLogIn.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
-#include<iomanip>
-#include<stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
 #include <Windows.h>
-#include<fstream>
+#include <fstream>
 #include <string.h>
-#include"userLog.h"
+#include "userLog.h"
 using namespace std;
 
 main()
 {
+    // functions
+    void displayAdminPanelMenu();
+
     coffee_list *menu;
     menu = makeCoffeeList();
     get_coffee_menu(menu);
@@ -16,25 +19,88 @@ main()
     List *user;
     user = makeEmptyList();
     readToList(user);
-    //displayList(user);
+    // displayList(user);
     int choice;
     do
     {
-        choice=loginOrSignup();
-    }while(choice<1 || choice>3);
-
-    while(choice>=1 && choice <=2)
-    {
-        if(choice==1)
+        cout << "1.Main Menu\n";
+        cout << "2.Admin Panel\n";
+        cout << "3.Exit\n";
+        cin >> choice;
+        switch (choice)
         {
-            LogInUser(user, menu);
-        }
-        else if(choice==2)
+        case 1:
         {
-            signUp(user, menu);
+            loginOrSignup();
+            while (choice >= 1 && choice <= 2)
+            {
+                if (choice == 1)
+                {
+                    LogInUser(user, menu);
+                }
+                else if (choice == 2)
+                {
+                    signUp(user, menu);
+                }
+                choice = loginOrSignup();
+            }
         }
-        choice=loginOrSignup();
-    }
+        break;
+        case 2:
+        {
+            int choice2;
+            displayAdminPanelMenu();
+            cin >> choice2;
+            switch (choice2)
+            {
+                //hllo
+            // create coffee
+            case 1:
+            {
+            }
+            break;
+            // read coffee
+            case 2:
+            {
+            }
+            break;
+            // edit coffee
+            case 3:
+            {
+            }
+            break;
+            // delete coffee
+            case 4:
+            {
+            }
+            break;
+            // best-seller
+            case 5:
+            {
+            }
+            break;
+            // total cash
+            case 6:
+            {
+            }
+            break;
+            case 7:
+            {
+                        }
+            break;
+            }
+        }
+        }
+    } while (choice != 3);
+}
 
-
+void displayAdminPanelMenu()
+{
+    cout << "1.Create\n";
+    cout << "2.Read\n";
+    cout << "3.Edit\n";
+    cout << "4.Delete\n";
+    cout << "5.Best-Seller Coffee\n";
+    cout << "6.Total Cash\n";
+    cout << "7.Exit\n";
 }
